a8b3605af363f2df9e65d99beec0863a47f73472
thechidubem <kingsleychidubem042@gmail.com>
2022-04-29 17:59:48 +0100

    connected to server

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,45 +1,54 @@
-import {TextInput, Text, View, StyleSheet} from "react-native";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
 import { GlobalStyles } from "../../constants/Styles";
 
-function Input({label, style, textInputConfig}) {
+function Input({ label, invalid, style, textInputConfig }) {
 
-    const inputStyles = [styles.input];
+  const inputStyles = [styles.input];
 
-    if (textInputConfig && textInputConfig.multiline) {
-        inputStyles.push(styles.inputMultiline)
-    }
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline)
+  }
 
-    return (
-        <View style={[styles.InputContainer, style]}>
-       <Text style={styles.label}>{label}</Text>
-       <TextInput style={styles.input} {...textInputConfig} />
-        </View>
-    );
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
+  }
+
+  return (
+    <View style={[styles.inputContainer, style]}>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
+    </View>
+  );
 }
 
 export default Input;
 
 const styles = StyleSheet.create({
-    InputContainer: {
-         marginHorizontal: 4,
-         marginVertical: 8,
-         flex: 1
-    },
-    label: {
-        fontSize: 12,
-        color: GlobalStyles.colors.primary100,
-        marginBottom: 4
-    },
-    input: {
-        backgroundColor: GlobalStyles.colors.primary100,
-        color: GlobalStyles.colors.primary700,
-        padding: 6,
-        borderRadius: 6,
-        fontSize: 18
-    },
-    inputMultiline: {
-        minHeight: 100,
-        textAlignVertical: "top",
-
-    }
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top'
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50
+  }
 });
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,68 +1,109 @@
-import { StyleSheet, View } from "react-native";
-import { useContext, useLayoutEffect } from "react";
-import IconButton from "../components/UI/IconButton";
-import { GlobalStyles } from "../constants/Styles";
-import Button from "../components/UI/Button";
-import { ExpensesContext } from "../store/Expense-context";
-import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { useContext, useLayoutEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-function ManageExpenses({route, navigation}) {
-const expensesCtx = useContext(ExpensesContext);
-const editedExpenseId = route.params?.expenseId;
-const isEditing = !!editedExpenseId;
+import ExpenseForm from '../components/ManageExpense/ExpenseForm';
+import ErrorOverlay from '../components/UI/ErrorOverlay';
+import IconButton from '../components/UI/IconButton';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
+import { GlobalStyles } from '../constants/Styles';
+import { ExpensesContext } from '../store/Expense-context';
+import { storeExpense, updateExpenses, deleteExpense } from '../util/http';
 
-useLayoutEffect(() => {
+function ManageExpense({ route, navigation }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState();
+
+  const expensesCtx = useContext(ExpensesContext);
+
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
+  useLayoutEffect(() => {
     navigation.setOptions({
-        title: isEditing ? "Edit Expense" : "Add Expense"
-    }) 
-}, [navigation, isEditing])
-  
-  function deleteExpenseHandler() {
-     expensesCtx.deleteExpense(editedExpenseId);
-     navigation.goBack();
-  };
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
+  async function deleteExpenseHandler() {
+    setIsSubmitting(true);
+    try {
+      await deleteExpense(editedExpenseId);
+      expensesCtx.deleteExpense(editedExpenseId);
+      navigation.goBack();
+    } catch(error) {
+        setError("Could not delete expense - please try again later")
+        setIsSubmitting(false)
+    }
+  }
 
   function cancelHandler() {
     navigation.goBack();
-  };
+  }
 
-  function confirmHandler(expenseData) {
-    if (isEditing) {
+  async function confirmHandler(expenseData) {
+    setIsSubmitting(true)
+    try {
+      if (isEditing) {
         expensesCtx.updateExpense(editedExpenseId, expenseData);
-    } else {
-        expensesCtx.addExpense(expenseData);
-    }
+        await updateExpenses(editedExpenseId, expenseData);
+      } else {
+        const id = await storeExpense(expenseData);
+        expensesCtx.addExpense({ ...expenseData, id: id });
+      }
       navigation.goBack();
-  };
+    } catch (error) {
+         setError("Could not save data - please try again later")
+         setIsSubmitting(false); 
+    }
+  }
 
- return (
-  <View style={styles.container}>
-         <ExpenseForm 
-         submitButtonLabel={isEditing ? "Update" : "Add"}
-         onSubmit={confirmHandler}
-          onCancel={cancelHandler} />
-    {isEditing && 
-    <View style={styles.deleteContainer}>
-        <IconButton icon="trash" 
-    color={GlobalStyles.colors.error500} size={36} onPress={deleteExpenseHandler}/>
-    </View>}
-  </View>
- )
+  if (error && isSubmitting) {
+    return <ErrorOverlay message={error} />
+  }
+
+  if (isSubmitting) {
+    return <LoadingOverlay />
+  }
+
+  return (
+    <View style={styles.container}>
+      <ExpenseForm
+        submitButtonLabel={isEditing ? 'Update' : 'Add'}
+        onSubmit={confirmHandler}
+        onCancel={cancelHandler}
+        defaultValues={selectedExpense}
+      />
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
-export default ManageExpenses;
+export default ManageExpense;
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       padding: 24,
-       backgroundColor: GlobalStyles.colors.primary800
-   },
-   deleteContainer: {
-       marginTop: 16,
-       paddingTop: 8,
-       borderTopWidth: 2,
-       borderTopColor: GlobalStyles.colors.primary200,
-       alignItems: "center"
-   }
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
 });
